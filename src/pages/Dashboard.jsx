5a7dc2261177367f05a994fcbdc9e8cd34b400ab
khizer12khoker <kingclash5080@gmail.com>
2026-05-08 11:08:41 +0500

    third

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -43,11 +43,12 @@ function Dashboard() {
       );
 
       setUser(res.data.user);
-      console.log(res.data);
 
     } catch (error) {
 
-      localStorage.removeItem("token");
+      localStorage.removeItem("accessToken");
+
+      localStorage.removeItem("refreshToken");
 
       navigate("/");
 
@@ -62,6 +63,8 @@ function Dashboard() {
 
     localStorage.removeItem("accessToken");
 
+    localStorage.removeItem("refreshToken");
+
     navigate("/");
 
   };
@@ -71,27 +74,28 @@ function Dashboard() {
 
   return (
 
-    <div className="flex justify-center items-center h-screen bg-gray-100">
+    <div className="min-h-screen bg-gray-100 p-10">
 
-      <div className="bg-white p-8 rounded-lg shadow-lg w-96">
+      <div className="bg-white p-8 rounded-lg shadow-lg">
 
-        <h1 className="text-3xl font-bold text-center mb-6">
+        <h1 className="text-3xl font-bold mb-6">
           Dashboard
         </h1>
+
 
         {
           user && (
             <div>
 
-              <p className="mb-3 text-lg">
+              <p className="mb-2">
                 Name: {user.name}
               </p>
 
-              <p className="mb-3 text-lg">
+              <p className="mb-2">
                 Email: {user.email}
               </p>
 
-              <p className="mb-5 text-lg">
+              <p className="mb-5">
                 Role: {user.role}
               </p>
 
@@ -99,9 +103,69 @@ function Dashboard() {
           )
         }
 
+
+
+
+        {
+          user?.role === "admin" && (
+
+            <div className="bg-red-100 p-5 rounded mb-5">
+
+              <h2 className="text-2xl font-bold mb-3">
+                Admin Dashboard
+              </h2>
+
+              <p>
+                Manage Users
+              </p>
+
+              <p>
+                Delete Users
+              </p>
+
+              <p>
+                View Reports
+              </p>
+
+            </div>
+
+          )
+        }
+
+
+
+
+        {
+          user?.role === "user" && (
+
+            <div className="bg-green-100 p-5 rounded mb-5">
+
+              <h2 className="text-2xl font-bold mb-3">
+                User Dashboard
+              </h2>
+
+              <p>
+                View Profile
+              </p>
+
+              <p>
+                Edit Account
+              </p>
+
+              <p>
+                View Orders
+              </p>
+
+            </div>
+
+          )
+        }
+
+
+
         <button
           onClick={logout}
-          className="bg-black text-white w-full p-3 rounded"
+          className="bg-black text-white px-6 py-3 rounded"
         >
           Logout
         </button>
